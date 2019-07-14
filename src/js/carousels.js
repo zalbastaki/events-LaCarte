04c309c8d4f03efbce2808f8bc993bc942457ef1
Zain Albastaki <zain.albastaki@gmail.com>
2019-07-14 12:33:54 +0100

    Add 404 and scroll to slider when img clicked

--- src/js/carousels.js
+++ src/js/carousels.js
@@ -24,4 +24,7 @@ $(".event-gallery-image-a").on("click", slickGoTo);
 function slickGoTo() {
     const index = $(this).data("index");
     $(".event-carousel").slick('slickGoTo', index);
+    $('html, body').animate({
+        scrollTop: $(".event-carousel").offset().top
+    }, 800);
 }
